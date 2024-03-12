3e8228b54b21a305428a7b3e90032fb92dbb8770
trollroll4u <yuvalron0@gmail.com>
2024-03-13 01:40:34 +0200

    deploying

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -3,8 +3,7 @@ module.exports = {
   apps: [
     {
       name: 'partypic-front',
-      script: 'vite',
-      args: 'build && vite preview --host 127.0.0.1 --port 443',
+      script: "vite preview --port 443 --host --config vite.prod.config.ts",
       env_production: {
         NODE_ENV: 'production',
       },
